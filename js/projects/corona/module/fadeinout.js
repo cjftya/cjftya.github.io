3eb598f676c11e1d3d9fcc82c0236a327e0d235c
cjftya <cjftya@gmail.com>
2019-06-03 05:52:09 +0900

    test4

--- js/projects/corona/module/fadeinout.js
+++ js/projects/corona/module/fadeinout.js
@@ -46,6 +46,6 @@ class FadeInOut {
 
     draw() {
         this.__c.applyColor();
-        rect(-20, -20, windowWidth + 20, windowHeight + 20);
+        rect(-80, -80, windowWidth + 80, windowHeight + 80);
     }
 }
