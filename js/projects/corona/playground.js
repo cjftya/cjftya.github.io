05ee4d746a5d10c995c40d9bd456478a56c91dd3
cjftya <cjftya@gmail.com>
2019-06-03 05:38:56 +0900

    add fade in-out effect

--- js/projects/corona/playground.js
+++ js/projects/corona/playground.js
@@ -1,4 +1,8 @@
-﻿function preload() {
+﻿var __fadeInOut = null;
+var __contentsScene = false;
+var __selectedContents = "";
+
+function preload() {
     //font = loadFont("https://cjftya.github.io/assets/font/Gabia-Solmee-Regular.ttf");
 
 }
@@ -6,18 +10,31 @@
 function setup() {
     createCanvas(windowWidth, windowHeight);
 
-    this.__pinLock = new PinLock(patternResolve);
+    this.__pinLock = new PinLock(patternResult);
     this.__particleCont = new ParticleController();
+    __fadeInOut = new FadeInOut(fadeInOutResult);
 }
 
 function draw() {
-    background(255,255, 255);
+    background(255, 255, 255);
     noStroke();
 
-    this.__particleCont.update();
-    this.__particleCont.draw();
+    if (!__contentsScene) {
+        this.__particleCont.update();
+        this.__particleCont.draw();
+        this.__pinLock.draw();
 
-    this.__pinLock.draw();
+    } else {
+        if (__selectedContents == "__myGirlPattern") {
+            fill(255, 0, 0, 50);
+            ellipse(100, 100, 50, 50);
+        } else if (__selectedContents == "__motherOfMyGirlPattern") {
+            fill(0, 0, 255, 50);
+            rect(100, 100, 100, 100);
+        }
+    }
+    __fadeInOut.update();
+    __fadeInOut.draw();
 }
 
 function mousePressed() {
@@ -36,6 +53,13 @@ function windowResized() {
     resizeCanvas(windowWidth, windowHeight);
 }
 
-function patternResolve(data) {
-    alert(data);
+function patternResult(data) {
+    __selectedContents = data;
+    __fadeInOut.fadeIn();
+}
+
+function fadeInOutResult(data) {
+    __contentsScene = true;
+    __fadeInOut.fadeOut();
+    console.log(data);
 }
