912282e93698c57637b3a14e335ecd1265ca39b4
cjftya <cjftya@gmail.com>
2020-01-05 13:18:56 +0900

    change logic of move page

--- js/playground.js
+++ js/playground.js
@@ -31,7 +31,7 @@ function drawFpsCount() {
 function mousePressed() {
     counter++;
     if(counter == 10) {
-       location.replace("projects/viola/index.html");        
+       location.href = "projects/viola/index.html";        
     }
     //    location.replace("projects/viola/index.html");
     //    location.replace("projects/letterbe/index.html");
