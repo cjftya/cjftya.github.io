8d7b157d2335d20f26efe85f403c4849368892f8
cjftya <cjftya@gmail.com>
2020-02-29 03:04:44 +0900

    edit map position

--- projects/weddingcard/js/playground.js
+++ projects/weddingcard/js/playground.js
@@ -499,7 +499,7 @@ function initializeWeddingContents() {
 
     mapView = new MapView(ResourcePath.MapImage)
         .setPos(0, locationTextView.getPos().y + 80)
-        .setCropSrcPos(((1560 - winSize[0]) / 2) - 50, 200)
+        .setCropSrcPos(((1560 - winSize[0]) / 2), 240)
         .setShortcutText("네이버지도 바로가기")
         .setCropSize(winSize[0], 250);
 
