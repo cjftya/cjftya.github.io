1a167bb968544ca013ac80f31fe974afd647f305
cjftya <cjftya@gmail.com>
2020-03-20 04:30:53 +0900

    change ellipse shape

--- js/module/effect/particle/shape/blurcircle.js
+++ js/module/effect/particle/shape/blurcircle.js
@@ -15,6 +15,8 @@ class BlurCircle {
         this.__blurColor[1].setAlpha(10);
 
         this.__life = 0;
+
+        this.__vertexMap = new Map();
     }
 
     setLife(v) {
@@ -29,14 +31,30 @@ class BlurCircle {
         this.__life += v;
     }
 
+    setupVertex(index, size) {
+        var vertex = [];
+        var count = 6;
+        var slice = (Math.PI * 2) / count;
+        for (var i = 0; i < count; i++) {
+            var vx = (Math.cos(slice * i) * size / 2);
+            var vy = (Math.sin(slice * i) * size / 2);
+            vertex.push(new Vector2d().set(vx, vy));
+        }
+        this.__vertexMap.set(index, vertex);
+    }
+
     setRadius(r) {
         this.__r = r;
+        this.setupVertex(0, r);
     }
 
     setRadiusWithBlur(r, bR1, bR2) {
         this.__r = r;
         this.__blurR[0] = bR1;
         this.__blurR[1] = bR2;
+        this.setupVertex(0, r);
+        this.setupVertex(1, bR1);
+        this.setupVertex(2, bR2);
     }
 
     getRadius() {
@@ -67,14 +85,27 @@ class BlurCircle {
         return this;
     }
 
+    drawEllipse(index) {
+        beginShape();
+        if (this.__vertexMap.get(index) != null) {
+            for (var i = 0; i < this.__vertexMap.get(index).length; i++) {
+                vertex(this.pos.x + this.__vertexMap.get(index)[i].x,
+                    this.pos.y + this.__vertexMap.get(index)[i].y);
+            }
+        }
+        endShape(CLOSE);
+    }
+
     draw() {
         // blur
         for (var i = 1; i >= 0; i--) {
             fill(this.__blurColor[i]);
-            ellipse(this.pos.x, this.pos.y, this.__blurR[i], this.__blurR[i]);
+            this.drawEllipse(i + 1);
+            // ellipse(this.pos.x, this.pos.y, this.__blurR[i], this.__blurR[i]);
         }
         // point
         fill(this.__color);
-        ellipse(this.pos.x, this.pos.y, this.__r, this.__r);
+        this.drawEllipse(0);
+        // ellipse(this.pos.x, this.pos.y, this.__r, this.__r);
     }
 }
